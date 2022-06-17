60d6daf8a3582da7104c034b449de5df4680a3aa
JeffAllison <itsjallison@gmail.com>
2022-06-17 12:48:23 -0700

    added delete controller in birds.js controller file

--- __tests__/birds.test.js
+++ __tests__/birds.test.js
@@ -61,6 +61,8 @@ describe('birds routes', () => {
   it('DELETE /birds/:id should delete a specific bird', async () => {
     const resp = await request(app).delete('/birds/2');
     expect(resp.status).toEqual(200);
+    const { body } = await request(app).get('/birds/2');
+    expect(body).toEqual('');
   });
 
   afterAll(() => {
